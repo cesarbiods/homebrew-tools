6ae7e63c865da3aa6d981b368288edf3eb9e367a
Cesar Rincon <cdrincon42@icloud.com>
2019-11-20 18:01:54 -0500

    Trying a different way to run.

--- homebrew-formula/microcli.rb
+++ homebrew-formula/microcli.rb
@@ -4,6 +4,11 @@ class Microcli < Formula
     url "https://us-east-1.artifactory.wexapps.com/artifactory/fleet-mobile-snapshots/com/wex/microcli/refactor-SNAPSHOT/microcli-refactor-20191119.165510-1.jar"
     version "1.0"
   def install
-      system 'java -jar microcli.jar'
+      bottle :microcli
+      depends_on :java => '1.8+'
+
+      libexec.install Dir['*']                                              
+      bin.write_jar_script libexec/'microcli.jar', 'microcli'
+      # system 'java -jar microcli.jar'
     end
   end
