509d86aaf457a84edf594252f6a62d776f528a00
Cesar Rincon <cdrincon42@icloud.com>
2019-11-20 14:22:06 -0500

    Maybe I don't need the mystery variable.

--- homebrew-formula/microcli.rb
+++ homebrew-formula/microcli.rb
@@ -1,9 +1,8 @@
 class Microcli < Formula
-    desc "<Dank bro>"
+    desc "Dank bro"
     homepage "https://bitbucket.wexapps.com/projects/MOB/repos/microcli/browse"
     url "https://us-east-1.artifactory.wexapps.com/artifactory/fleet-mobile-snapshots/com/wex/microcli/refactor-SNAPSHOT/microcli-refactor-20191119.165510-1.jar"
     version "1.0"
-    sha256 "<sha>"
   def install
       system 'java -jar microcli.jar'
     end
