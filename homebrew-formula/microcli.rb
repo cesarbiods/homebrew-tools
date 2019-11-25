1dee3a996414f308a072601d7cedcea960157d38
Cesar Rincon <cdrincon42@icloud.com>
2019-11-24 20:57:09 -0500

    Make sure it still works or it'll python me.

--- homebrew-formula/microcli.rb
+++ homebrew-formula/microcli.rb
@@ -1,14 +1,18 @@
 class Microcli < Formula
-    desc "Dank bro"
-    homepage "https://bitbucket.wexapps.com/projects/MOB/repos/microcli/browse"
-    url "https://us-east-1.artifactory.wexapps.com/artifactory/fleet-mobile-snapshots/com/wex/microcli/refactor-SNAPSHOT/microcli-refactor-20191119.165510-1.jar"
-    version "1.0"
-    bottle :microcli
-    # depends_on :openjdk => '1.8+'
+  desc "Dank bro"
+  homepage "https://bitbucket.wexapps.com/projects/MOB/repos/microcli/browse"
+  url "https://us-east-1.artifactory.wexapps.com/artifactory/fleet-mobile-snapshots/com/wex/microcli/refactor-SNAPSHOT/microcli-refactor-20191119.165510-1.jar"
+  sha256 "4763aaa12c58ae29354953b4e308854d3cefb9222c6262cf788b79ac50b68cf7"
+  version "1.0"
+  bottle :microcli
+  # depends_on :openjdk => '1.8+'
     
   def install
-      libexec.install Dir['*']
-      bin.write_jar_script libexec/'microcli.jar', 'microcli'
-      # system 'java -jar microcli.jar'
-    end
+    # inreplace "brew/srcclr", "##PREFIX##", "#{prefix}"
+    # prefix.install "srcclr-console.jar"
+    # bin.install "brew/srcclr"
+    libexec.install Dir['*']
+    bin.write_jar_script libexec/'microcli.jar', 'microcli'
+    # system 'java -jar microcli.jar'
   end
+end
