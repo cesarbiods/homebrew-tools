b255d708ac10b91f5c2c92e461604e2bb91c6e63
Cesar Rincon <cdrincon42@icloud.com>
2019-12-13 16:10:13 -0500

    Suggestions.

--- Formula/microcli.rb
+++ Formula/microcli.rb
@@ -4,8 +4,8 @@ class Microcli < Formula
   url "https://us-east-1.artifactory.wexapps.com/artifactory/fleet-mobile-snapshots/com/wex/microcli/refactor-SNAPSHOT/microcli-refactor-20191119.165510-1.jar"
   sha256 "4763aaa12c58ae29354953b4e308854d3cefb9222c6262cf788b79ac50b68cf7"
   version "1.0"
-  bottle :microcli
-    
+  bottle :unneeded
+
   def install
     libexec.install Dir['*']
     bin.write_jar_script libexec/'microcli.jar', 'microcli'
