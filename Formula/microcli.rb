0ca23730368818e7bd711d333844f211c3804c0a
Cesar Rincon <cdrincon42@icloud.com>
2020-01-14 09:50:35 -0500

    Updated checksum.

--- Formula/microcli.rb
+++ Formula/microcli.rb
@@ -2,7 +2,7 @@ class Microcli < Formula
   desc "Dank bro"
   homepage "https://bitbucket.wexapps.com/projects/MOB/repos/microcli/browse"
   url "https://us-east-1.artifactory.wexapps.com/artifactory/fleet-mobile-releases/com/wex/microcli/1.2.0/microcli-1.2.0.jar"
-  sha256 "4763aaa12c58ae29354953b4e308854d3cefb9222c6262cf788b79ac50b68cf7"
+  sha256 "cf58a4f07f9b72a6627688b4b771cefd7bf67010e093c719a251270f89a96841"
   version "1.2"
   bottle :unneeded
 
