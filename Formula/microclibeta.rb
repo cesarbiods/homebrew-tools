e605c5c42550d258659f64f83efa0faff9991a33
Cesar Rincon <cesar.rincon@wexinc.com>
2020-03-08 21:26:26 -0400

    Trying something new.

--- Formula/microclibeta.rb
+++ Formula/microclibeta.rb
@@ -1,3 +1,16 @@
+class JavaRequirement < Requirement
+  fatal true
+
+  satisfy(:build_env => false) { which("corretto") }
+
+  def message; <<~EOS
+    Java corretto is required; install it via one of:
+      brew install r
+      brew cask install r-app
+    EOS
+  end
+end
+
 class Microclibeta < Formula
   desc "Microcli is a CLI tool for managing AWS Cloud Formation Stacks"
   homepage "https://bitbucket.wexapps.com/projects/MOB/repos/microcli/browse"
@@ -5,7 +18,7 @@ class Microclibeta < Formula
   sha256 "fd64a1e9be67232049a6b9a26ed5789deccc8e3b70f2d649e0eccbda8ff3094b"
   version "2.4"
   bottle :unneeded
-  depends_on :corretto
+  depends_on JavaRequirement
 
   def install
     libexec.install Dir['*']
